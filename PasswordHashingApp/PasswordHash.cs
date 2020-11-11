e2d432b724b19e785a4e388e2ba7a577b30ed090
venushar <vrajapaksha@outlook.com>
2020-11-11 16:04:23 +0530

    initial commit
    initial commit

---
+++ PasswordHashingApp/PasswordHash.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PasswordHashingApp
+{
+    class PasswordHash
+    {
+        /// <summary>Creates the password hash.</summary>
+        /// <param name="password">The password.</param>
+        /// <param name="passwordHash">The password hash.</param>
+        /// <param name="passwordSalt">The password salt.</param>
+        public void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using var hmac = new System.Security.Cryptography.HMACSHA512();
+            passwordSalt = hmac.Key;
+            passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+        }
+
+        /// <summary>Verifies the password hash.</summary>
+        /// <param name="password">The password.</param>
+        /// <param name="passwordHash">The password hash.</param>
+        /// <param name="passwordSalt">The password salt.</param>
+        /// <returns></returns>
+        public bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            ///<summary>same as create password hash pass passwordsalt</summary>
+            using (var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt))
+            {
+                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                for (int i = 0; i < computedHash.Length; i++)
+                {
+                    ///<summary>compair both computed and password hashes </summary>
+                    if (computedHash[i] != passwordHash[i]) return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ PasswordHashingApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace PasswordHashingApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*****************Password Hashing*****************");
+
+            PasswordHash passwordHashObj = new PasswordHash();
+            byte[] passwordHash;
+            byte[] passwordSalt;
+            passwordHashObj.CreatePasswordHash("1qaz2wsx@", out passwordHash, out passwordSalt);
+            Console.WriteLine("*****************Create hash and salt*****************");
+            Console.WriteLine("check the password has and salt for password 1qaz2wsx@" );           
+            Console.WriteLine("passwordHash=" + Convert.ToBase64String(passwordHash));
+            Console.WriteLine("passwordSalt =" + Convert.ToBase64String(passwordSalt));
+            Console.WriteLine("******************verify the password ***************");
+
+            Console.WriteLine(passwordHashObj.VerifyPasswordHash("1qaz2wsx@", passwordHash, passwordSalt));
+        }
+
+
+    }
+}
